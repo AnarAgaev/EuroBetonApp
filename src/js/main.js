e5edf9264f3dba09ab33a0bf7565b3d7775fbf67
Anar Agaev <anar.n.agaev@gmail.com>
2020-09-27 22:31:52 +0300

    added news page + adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,9 @@ document.addEventListener("DOMContentLoaded",() => {
   const body = document.body;
   const dropLinks = document.querySelectorAll(".products__link.drop");
   const navLinks = document.querySelectorAll(".nav__link.drop");
+  const faqLinks = document.getElementsByClassName("faq__item");
   let windowWidth = window.innerWidth;
+
 
   window.addEventListener('resize', (event) => {
     windowWidth = window.innerWidth;
@@ -191,55 +193,96 @@ document.addEventListener("DOMContentLoaded",() => {
 
 
 
+// FAQ accordion
+  const closeFaqAccordion = () => {
+    for (let i = 0; i < faqLinks.length; ++i) {
+      faqLinks[i].classList.remove("visible");
+    }
+  };
+
+  for (let i = 0; i < faqLinks.length; ++i) {
+    faqLinks[i].addEventListener('click', event => {
+      let li = event.target.closest('li');
+      let isVisible = li.classList.contains('visible');
+
+      closeFaqAccordion();
+
+      isVisible
+        ? li.classList.remove('visible')
+        : li.classList.add('visible');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+
+
+
 
 
 });
 
 
-// ***Янадекс нары -- Начало
+
+
+
+
+
+if (document.getElementById("map")) {
+  // ***Янадекс нары -- Начало
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-function init(){
-  // Создание карты.
-  let myMap = new ymaps.Map("map", {
-      // Координаты центра карты.
-      // Порядок по умолчанию: «широта, долгота».
-      // Чтобы не определять координаты центра карты вручную,
-      // воспользуйтесь инструментом Определение координат.
-      center: [54.90063335092027,51.65889470515492],
-      // Уровень масштабирования. Допустимые значения:
-      // от 0 (весь мир) до 19.
-      zoom: 4,
-      controls: ['fullscreenControl', 'zoomControl'],
-    }),
+  ymaps.ready(init);
+  function init(){
+    // Создание карты.
+    let myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [54.90063335092027,51.65889470515492],
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 4,
+        controls: ['fullscreenControl', 'zoomControl'],
+      }),
 
-    myPlacemarkAsfaltobeton = new ymaps.Placemark([55.76, 37.64], {}, {
-      iconLayout: 'default#image',
-      iconImageHref: '/img/placemark-asfaltobeton.png',
-      iconImageSize: [200, 43],
-      iconImageOffset: [-16, -43]
-    }),
+      myPlacemarkAsfaltobeton = new ymaps.Placemark([55.76, 37.64], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/img/placemark-asfaltobeton.png',
+        iconImageSize: [200, 43],
+        iconImageOffset: [-16, -43]
+      }),
 
-    myPlacemarkEkaterinburg = new ymaps.Placemark([56.78875104810377,60.60157099999987], {}, {
-      iconLayout: 'default#image',
-      iconImageHref: '/img/placemark-ekaterinburg.png',
-      iconImageSize: [236, 43],
-      iconImageOffset: [-15, -43]
-    }),
+      myPlacemarkEkaterinburg = new ymaps.Placemark([56.78875104810377,60.60157099999987], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/img/placemark-ekaterinburg.png',
+        iconImageSize: [236, 43],
+        iconImageOffset: [-15, -43]
+      }),
 
-    myPlacemarkLipeck = new ymaps.Placemark([52.60358425779388,39.59623749999995], {}, {
-      iconLayout: 'default#image',
-      iconImageHref: '/img/placemark-lipeck.png',
-      iconImageSize: [183, 43],
-      iconImageOffset: [-15, -43]
-    });
+      myPlacemarkLipeck = new ymaps.Placemark([52.60358425779388,39.59623749999995], {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/img/placemark-lipeck.png',
+        iconImageSize: [183, 43],
+        iconImageOffset: [-15, -43]
+      });
 
-  myMap.geoObjects
-    .add(myPlacemarkAsfaltobeton)
-    .add(myPlacemarkEkaterinburg)
-    .add(myPlacemarkLipeck);
+    myMap.geoObjects
+      .add(myPlacemarkAsfaltobeton)
+      .add(myPlacemarkEkaterinburg)
+      .add(myPlacemarkLipeck);
 
-  myMap.behaviors.disable('scrollZoom');
-}
+    myMap.behaviors.disable('scrollZoom');
+  }
 // ***Янадекс нары -- Конец
+}
